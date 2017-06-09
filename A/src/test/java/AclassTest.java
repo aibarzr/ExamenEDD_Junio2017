baaa3ffe0560d2ac93a6206c86439ef43bb1a283
Alejandro Ibarz <aibarzr@live.com>
2017-06-09 10:48:56 +0200

    Commit 02

--- A/src/test/java/AclassTest.java
+++ A/src/test/java/AclassTest.java
@@ -2,9 +2,19 @@ import java.awt.Point;
 
 import org.junit.Assert;
 import org.junit.Test;
+import org.mockito.Mock;
 
 public class AclassTest {
 	Aclass obj = new Aclass();
+	
+	@Mock
+	FaultAction faultAction;
+	
+	@Mock
+	Button button;
+	
+	@Mock
+	Point point;
 	
 	@Test
 	public void locateTest() {
@@ -21,6 +31,46 @@ public class AclassTest {
 	@Test
 	//Triangle
 	public void getAreaTest() {
-		Assert.assertEquals(2, Aclass.getArea(2, 2, 2));
+		Assert.assertEquals(2, Aclass.getArea(2, 2, 2), 2);
+		try {
+			Assert.assertEquals(4, Aclass.getArea(2, 2, 4), 4);
+		} catch (RuntimeException e) {
+			e.printStackTrace();
+		}
+		try {
+			Assert.assertEquals(4, Aclass.getArea(4, 2, 2), 4);
+		} catch (RuntimeException e) {
+			e.printStackTrace();
+		}
+		try {
+			Assert.assertEquals(4, Aclass.getArea(2, 4, 2), 4);
+		} catch (RuntimeException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	@Test
+	public void concatTest() {
+		Assert.assertEquals("en un lugar de la mancha", obj.concat(true, "En un lugar", " de la Mancha"));
+		try {
+			Assert.assertEquals(" de la mancha", obj.concat(true, null, " de la Mancha"));
+		} catch (RuntimeException e) {
+			e.printStackTrace();
+		}
+		try {
+			Assert.assertEquals("en un lugar", obj.concat(true, "En un lugar", null));
+		} catch (RuntimeException e) {
+			e.printStackTrace();
+		}
+		try {
+			Assert.assertEquals(null, obj.concat(true, null, null));
+		} catch (RuntimeException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	@Test
+	public void mockito1Test() {
+		
 	}
 }
